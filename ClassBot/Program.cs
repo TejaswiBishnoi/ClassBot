4f0a5f0aef1143324be42daffbc2ac0068ed2620
Tejaswi Bishnoi <62018605+TejaswiBishnoi@users.noreply.github.com>
2022-01-17 18:26:20 +0530

    Made a separate reusable login function.

--- ClassBot/Program.cs
+++ ClassBot/Program.cs
@@ -13,32 +13,23 @@ namespace ClassBot
     {
         static void Main(string[] args)
         {
-            var driver = new ChromeDriver();
-            driver.Navigate().GoToUrl("https://lms.iitjammu.ac.in");
+            var Obj = new Worker();
+
+            Obj.driver.Navigate().GoToUrl("https://lms.iitjammu.ac.in");
             Thread.Sleep(1000);
-            var LogIn = driver.FindElement(By.CssSelector(@"a[href='https://lms.iitjammu.ac.in/login/index.php']"));
+            var LogIn = Obj.driver.FindElement(By.CssSelector(@"a[href='https://lms.iitjammu.ac.in/login/index.php']"));
             LogIn.Click();
             Thread.Sleep(1000);
-            var UsrNam = driver.FindElement(By.Name("username"));
-            var PasWrd = driver.FindElement(By.Name("password"));
-            var LgnBtn = driver.FindElement(By.Id("loginbtn"));
+            Obj.Login();
 
-            UsrNam.SendKeys("2020UCS0084");
-            PasWrd.SendKeys("Iam100%dehru");
-            LgnBtn.Click();
-
-            driver.Navigate().GoToUrl("https://lms.iitjammu.ac.in/course/view.php?id=324");
+            Obj.driver.Navigate().GoToUrl("https://lms.iitjammu.ac.in/course/view.php?id=324");
             Thread.Sleep(1000);
 
-            var ZoomLink = driver.FindElement(By.XPath("//*[contains(text(), 'click here')]"));
+            var ZoomLink = Obj.driver.FindElement(By.XPath("//*[contains(text(), 'click here')]"));
             string link = ZoomLink.GetAttribute("href");
-            driver.Navigate().GoToUrl(link);
+            Obj.driver.Navigate().GoToUrl(link);
             Thread.Sleep(5000);
-
-            driver.SwitchTo().Alert().Accept();
-
-            Thread.Sleep(10000);
-            driver.Quit();
+            Obj.driver.Quit();
         }
     }
 }
---
+++ ClassBot/Worker.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace ClassBot
+{
+    class Worker
+    {
+        public ChromeDriver driver = new ChromeDriver();
+        public void Login()
+        {
+            var UsrNam = driver.FindElement(By.Name("username"));
+            var PasWrd = driver.FindElement(By.Name("password"));
+            var LgnBtn = driver.FindElement(By.Id("loginbtn"));
+
+            UsrNam.SendKeys("2020UCS0084");
+            PasWrd.SendKeys("Iam100%dehru");
+            LgnBtn.Click();
+        }
+    }
+}
